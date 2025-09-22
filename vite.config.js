253407246635af171a395662833f6a7d333fe74e
Byron Wheeler <bwheel1990@gmail.com>
2025-09-21 20:51:05 -0500

    deploying via workflow now

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import { viteStaticCopy } from 'vite-plugin-static-copy';
 export default defineConfig({
   base:"sec_exchange_metrics",
   publicDir: 'public',
-  build: {
-    outDir: 'docs/',
-    emptyOutDir: true,
-  },
   plugins: [
     viteStaticCopy({
       targets: [
