99071915d45e56e514b73a1ec90d2f9fe7733cc4
Byron Wheeler <bwheel1990@gmail.com>
2025-09-21 20:48:22 -0500

    trying new base url

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
  
 export default defineConfig({
+  base:"sec_exchange_metrics",
   publicDir: 'public',
   build: {
     outDir: 'docs/',
